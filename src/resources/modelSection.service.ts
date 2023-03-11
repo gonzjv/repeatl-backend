45d9a8487a37c4c7bd8a1152c84a669d6476871b
gonzjv <gonzjv@ya.ru>
2023-03-11 19:18:16 +0300

    feat: get completed model sections

--- src/resources/modelSection.service.ts
+++ src/resources/modelSection.service.ts
@@ -1,29 +1,29 @@
 import { repeatlDataSource } from '../../app-data-source';
 import { Collection } from '../entity/collection.entity';
 import { ModelSection } from '../entity/modelSection.entity';
+import { ModelSectionState } from '../entity/modelSectionState.entity';
 
 const collectionRepo =
   repeatlDataSource.getRepository(Collection);
 const modelSectionRepo =
   repeatlDataSource.getRepository(ModelSection);
+const modelSectionStateRepo =
+  repeatlDataSource.getRepository(ModelSectionState);
 
 const addModelSection = async (
   collectionId: number,
   sectionData: { number: string }
 ) => {
-  const collection =
-    await collectionRepo.findOneBy({
-      id: collectionId,
-    });
+  const collection = await collectionRepo.findOneBy({
+    id: collectionId,
+  });
 
   const modelSection = modelSectionRepo.create({
     number: sectionData.number,
     collection: collection!,
   });
 
-  const results = await modelSectionRepo.save(
-    modelSection
-  );
+  const results = await modelSectionRepo.save(modelSection);
   return results;
 };
 
@@ -39,4 +39,22 @@ const getModelSection = async (
     },
   });
 
-export { addModelSection, getModelSection };
+const getCompletedModelSectionArr = async (
+  userId: number
+) =>
+  await modelSectionStateRepo.find({
+    where: {
+      collectionState: {
+        courseState: {
+          progress: { user: { id: userId } },
+        },
+      },
+      isCompleted: true,
+    },
+  });
+
+export {
+  addModelSection,
+  getModelSection,
+  getCompletedModelSectionArr,
+};
